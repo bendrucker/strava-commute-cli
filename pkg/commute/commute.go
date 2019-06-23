d1b8edc6f511f52f981f8915dbe948ce27bd303c
Ben Drucker <bvdrucker@gmail.com>
2019-06-22 17:29:34 -0700

    add Label method for commutes

--- pkg/commute/commute.go
+++ pkg/commute/commute.go
@@ -18,3 +18,17 @@ type Commute struct {
 func (c Commute) Duration() time.Duration {
 	return time.Duration(c.Distance.Nanometers() / c.Speed.MetersPerSecond())
 }
+
+func noon(t time.Time) time.Time {
+	year, month, day := t.Date()
+	return time.Date(year, month, day, 12, 0, 0, 0, t.Location())
+}
+
+// Label returns a Morning or Evening label
+func (c Commute) Label() string {
+	if c.Start.Before(noon(c.Start)) {
+		return "Morning"
+	}
+
+	return "Evening"
+}
--- pkg/commute/commute_test.go
+++ pkg/commute/commute_test.go
@@ -17,3 +17,22 @@ func TestCommuteDuration(t *testing.T) {
 
 	assert.Equal(t, time.Duration(40)*time.Minute, duration)
 }
+
+func createTime(str string) time.Time {
+	t, _ := time.Parse(time.RFC822Z, str)
+	return t
+}
+
+func TestCommuteLabel(t *testing.T) {
+	morning := Commute{
+		Start: createTime("22 Jun 19 09:18 -0700"),
+	}
+
+	assert.Equal(t, morning.Label(), "Morning")
+
+	evening := Commute{
+		Start: createTime("22 Jun 19 18:18 -0700"),
+	}
+
+	assert.Equal(t, evening.Label(), "Evening")
+}
